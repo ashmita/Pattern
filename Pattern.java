cae956ba3363ed15b86618834bf34386e6411a71
Ashmita Kunwar <Ashmita_0321@deerwalk.edu.np>
2015-01-17 21:51:46 +0545

    first commit

---
+++ Pattern.java
@@ -0,0 +1,20 @@
+import java.security.PublicKey;
+
+/**
+ * Created by User on 1/17/2015.
+ */
+public class Pattern {
+    public static void main(String[] Arg)
+    {
+        System.out.println("          \'  \'");
+        System.out.println("           __ ");
+        System.out.println("          (\u00A9\u00A9)");
+        System.out.println("  /=======\\/");
+        System.out.println(" /|| %% ||");
+        System.out.println("* ||----||");
+        System.out.println("  \u00A5\u00A5----¥¥");
+        System.out.println("  \"\"    \"\"");
+
+    }
+
+}
